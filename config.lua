914e3257c81cf36e418a659a53e3ce7226a538a9
MillerInc <58441682+MillerInc@users.noreply.github.com>
2021-11-16 15:29:27 -0500

    Update config.lua
    updated config

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 Config = {}
 
 Config.Jobs = {
-    ['police'] = vector3(448.4, -973.2, 30.6),
-    ['ambulance'] = vector3(310.1, -599.43, 43.29),
+    ['police'] = vector3(461.33, -988.89, 30.69), --LSPD
+    ['ambulance'] = vector3(270.5, -1363.0, 24.5),
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
